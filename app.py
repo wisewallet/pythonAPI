577f0410b3371e81bb450b76104b61696952bf30
William Brower <will@mywisewallet.com>
2018-11-08 19:41:01 -0500

    testing

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import boto3
 
 app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb')
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    companyTable = dynamodb.Table('companies')
+    companyTableResponse = companyTable.scan()
+    companyList = companyTableResponse['Items']
+    while 'LastEvaluatedKey' in companyList:
+        companyTableResponse = companyTable.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        companyList.extend(companyTableResponse['Items'])
+    return companyList
 
 if __name__ == '__main__':
     app.run(debug=True)
