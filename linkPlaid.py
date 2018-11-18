75438782ea3c3725b429d7e476b81be7bdc9f9bc
William Brower <will@mywisewallet.com>
2018-11-18 17:21:06 -0500

    a

--- linkPlaid.py
+++ linkPlaid.py
@@ -37,10 +37,10 @@ def linkPlaid():
     # print(companyDicionaryDB.scan())
     data = json.loads(request.data)
     access_token = "access-development-65c2bdad-21b0-47e5-b1c5-f1455b83c340"
-    #public_token = data["public_token"]
-    #exchange_response = plaidClient.Item.public_token.exchange(public_token)
-    #print 'access token: ' + exchange_response['access_token']
-    #print 'item ID: ' + exchange_response['item_id']
+    # public_token = data["public_token"]
+    # exchange_response = plaidClient.Item.public_token.exchange(public_token)
+    # print 'access token: ' + exchange_response['access_token']
+    # print 'item ID: ' + exchange_response['item_id']
     transactions_response = plaidClient.Transactions.get(access_token,
                                                          start_date='2018-01-01',
                                                          end_date='2018-02-01')
@@ -56,6 +56,7 @@ def linkPlaid():
     end = idk.time()
     # print(transactions)
     for i in range(6):
-        print("Start: " +
-              '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-30)))
+        next_month = (datetime.now() + timedelta(-30 * (1 + i)).replace(
+            day=28) + datetime.timedelta(days=4)  # this will never fail
+        end_date=next_month - datetime.timedelta(days=next_month.day)
     return str(end - start)
