db89d690afe074d71db2b649826edfcd7ea18b15
William Brower <will@mywisewallet.com>
2018-11-18 15:58:29 -0500

    a

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, request
-from flask_pymongo import PyMongo
 import simplejson as json
 from signup import signup_api
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/"
-mongo = PyMongo(app)
+app = Flask(__name__)"
 
 app.register_blueprint(signup_api)
 
 @app.route('/', methods=['GET'])
 def index():
-    mongo.db.users.insert({"email": "main"})
     return json.dumps("Hello World")
 
 if __name__ == '__main__':
--- signup.py
+++ signup.py
@@ -1,7 +1,13 @@
 from flask import Blueprint
+import pymongo
 
 signup_api = Blueprint('signup_api', __name__)
 
+client = pymongo.MongoClient("mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/")
+db = client["users"]
+
 @signup_api.route("/signup")
 def signup():
+    for x in db.users.find():
+        print(x)
     return "list of accounts"
