e4c3cecd067eaf7beb016ce0afbe2a66c045eb32
William Brower <will@mywisewallet.com>
2018-11-18 15:26:47 -0500

    no clue

--- endpoints/signup.py
+++ endpoints/signup.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 signup_api = Blueprint('signup_api', __name__)
 
-@account_api.route("/signup")
+@signup_api.route("/signup")
 def signup():
     return "list of accounts"
