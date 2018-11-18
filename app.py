d6787b7848043404de4db08689c25e41b3311c61
William Brower <will@mywisewallet.com>
2018-11-18 16:40:36 -0500

    a

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ from linkPlaid import linkPlaid_api
 app = Flask(__name__)
 
 app.register_blueprint(signup_api)
-app.register_blueprint(linkPlaid_api
+app.register_blueprint(linkPlaid_api)
+
 
 @app.route('/', methods=['GET'])
 def index():
     return json.dumps("Hello World")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
