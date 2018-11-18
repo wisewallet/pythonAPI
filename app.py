dd512d3c93ee107780527b17924f6b7e560df8e6
William Brower <will@mywisewallet.com>
2018-11-18 15:20:59 -0500

    test

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 from flask import Flask, request
-import boto3
 import simplejson as json
 
 app = Flask(__name__)
-dynamodb = boto3.resource('dynamodb')
-
-def totalScore(company):
-    return company['eScore'] + company['sScore'] + company['gScore']
 
 @app.route('/getSuggestions', methods=['GET'])
 def index():
-    companyTable = dynamodb.Table('companies')
-    companyTableResponse = companyTable.scan()
-    companyList = companyTableResponse['Items']
-    while 'LastEvaluatedKey' in companyList:
-        companyTableResponse = companyTable.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-        companyList.extend(companyTableResponse['Items'])
-    companyList.sort(key=totalScore, reverse=True)
-    return json.dumps(companyList[:10])
+    return json.dumps("Hello World")
 
 if __name__ == '__main__':
     app.run(debug=True)
