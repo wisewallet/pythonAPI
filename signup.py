15f41d2187bb1b7b923967b30a8303b47a3d6f9c
William Brower <will@mywisewallet.com>
2018-11-18 16:08:33 -0500

    a

--- signup.py
+++ signup.py
@@ -7,7 +7,7 @@ client = pymongo.MongoClient("mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.
 db = client["users"]
 
 @signup_api.route("/signup")
-def signup():
+def signup(request):
     for x in db.users.find():
         print(x)
     return request
