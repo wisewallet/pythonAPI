75f4c126a3615daeab05f737adc6a7b1920200d7
William Brower <will@mywisewallet.com>
2018-11-18 16:51:40 -0500

    a

--- linkPlaid.py
+++ linkPlaid.py
@@ -3,20 +3,41 @@ import simplejson as json
 import pymongo
 import bcrypt
 import time
+import plaid
 
 linkPlaid_api = Blueprint('linkPlaid_api', __name__)
 
-client = pymongo.MongoClient(
+mongoClient = pymongo.MongoClient(
     "mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/")
-db = client["users"]
+db = mongoClient["users"]
+
+plaidClient = plaid.Client('5a9f59e5bdc6a4062cd4229b',
+                           '0f2c037ebe121dfeffc15cd13bb5f7',
+                           '06812b585d6f3b0ebde352a7759bb1',
+                           'development')
 
 
 @linkPlaid_api.route("/link")
 def linkPlaid():
     start = time.time()
     data = json.loads(request.data)
-    user = {"email": data['email'], "password": bcrypt.hashpw(data['password'], bcrypt.gensalt(
-    )), "linked_plaid": False, "first_name": data['firstName'], "last_name": data['lastName']}
-    toReturn = db.users.insert_one(user).inserted_id
+    access_token = "access-development-65c2bdad-21b0-47e5-b1c5-f1455b83c340"
+    #public_token = data["public_token"]
+    #exchange_response = plaidClient.Item.public_token.exchange(public_token)
+    #print 'access token: ' + exchange_response['access_token']
+    #print 'item ID: ' + exchange_response['item_id']
+    transactions_response = client.Transactions.get(access_token,
+                                                    start_date='2018-01-01',
+                                                    end_date='2018-02-01')
+    transactions = transactions_response['transactions']
+    while len(transactions) < transactions_response['total_transactions']:
+        transactions_response = client.Transactions.get(access_token,
+                                                        start_date='2018-01-01',
+                                                        end_date='2018-02-01',
+                                                        offset=len(
+                                                            transactions)
+                                                        )
+        transactions.extend(transactions_response['transactions'])
     end = time.time()
+    print(transactions)
     return str(end - start)
