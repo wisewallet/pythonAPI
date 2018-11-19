b65dc881881bc7a5710a4e60d66f7d06fa5eafee
William Brower <will@mywisewallet.com>
2018-11-19 12:10:36 -0500

    a

--- linkPlaid.py
+++ linkPlaid.py
@@ -51,6 +51,7 @@ def calculateScore(transactions):
             if attempt['transactionString'] in transactions[i]['name'].lower():
                 found = True
                 item = companyDB.companies.find_one({'name': attempt['matchName']})
+                companyDB.companies.update_one({'name': attempt['matchName']}, {"$addToSet": {"transactionString": transactions[i]['name']}})
                 foundCount += 1
                 scores['environmental'] += int(item['eScore'])
                 scores['social'] += int(item['sScore'])
