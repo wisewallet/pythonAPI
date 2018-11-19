18156ed1d78388bd7831daad47f386e550145238
William Brower <will@mywisewallet.com>
2018-11-18 20:36:50 -0500

    a

--- login.py
+++ login.py
@@ -14,6 +14,7 @@ db = client["users"]
 def login():
     data = json.loads(request.data)
     item = db.users.find_one({'email': data['email']})
+    print(item)
     if bcrypt.checkpw(item['password'], data['password']):
         return("It Matches!")
     else:
