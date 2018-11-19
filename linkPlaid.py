829297d2bebc13245bdfa0c4418233d624cd066f
William Brower <will@mywisewallet.com>
2018-11-18 21:49:59 -0500

    aa

--- linkPlaid.py
+++ linkPlaid.py
@@ -2,39 +2,36 @@ from flask import Blueprint, request
 import simplejson as json
 import pymongo
 import bcrypt
-import time as idk
+import time
 import plaid
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, date, timedelta
 import boto3
 from bson.objectid import ObjectId
 
 linkPlaid_api = Blueprint('linkPlaid_api', __name__)
 
-mongoClient = pymongo.MongoClient(
-    "mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/")
+mongoURI = "mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/"
+mongoClient = pymongo.MongoClient(mongoURI)
 db = mongoClient["users"]
 companyDB = mongoClient["companies"]
-plaidClient = plaid.Client('5a9f59e5bdc6a4062cd4229b',
-                           '0f2c037ebe121dfeffc15cd13bb5f7',
-                           '06812b585d6f3b0ebde352a7759bb1',
-                           'development')
+client_id = '5a9f59e5bdc6a4062cd4229b'
+secret = '0f2c037ebe121dfeffc15cd13bb5f7'
+public_key = '06812b585d6f3b0ebde352a7759bb1'
+environment = 'development'
+plaidClient = plaid.Client(client_id, secret, public_key, environment)
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 
-table = dynamodb.Table('companies')
 companyDicionaryDB = dynamodb.Table('companyDictionary')
-notFoundTable = dynamodb.Table('notFound')
 dictResponse = companyDicionaryDB.scan()
 companyDictionary = dictResponse['Items']
 while 'LastEvaluatedKey' in dictResponse:
-    response = companyDicionaryDB.scan(
-        ExclusiveStartKey=response['LastEvaluatedKey'])
+    response = companyDicionaryDB.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
     companyDictionary.extend(response['Items'])
 
 
 def get_first_day(dt, d_years=0, d_months=0):
-    # d_years, d_months are "deltas" to apply to dt
     y, m = dt.year + d_years, dt.month + d_months
     a, m = divmod(m - 1, 12)
     return date(y + a, m + 1, 1)
@@ -47,17 +44,13 @@ def get_last_day(dt):
 def calculateScore(transactions):
     scores = {'environmental': 0, 'social': 0, 'governance': 0, "politics": 0}
     foundCount = 0
-    foundTransactions = dict()
     found = False
     notFound = 0
     for i in range(len(transactions)):
         for attempt in companyDictionary:
             if attempt['transactionString'] in transactions[i]['name'].lower():
                 found = True
-                #print("Found: " + attempt['matchName'] + " as " + transactions[i]['name'])
-                item = companyDB.companies.find_one({'name':attempt['matchName']})
-                print("Found: " + transactions[i]['name'] + " as "+attempt['matchName'])
-                print("Found from MongoDB: "+ item['name'])
+                item = companyDB.companies.find_one({'name': attempt['matchName']})
                 foundCount += 1
                 scores['environmental'] += int(item['eScore'])
                 scores['social'] += int(item['sScore'])
@@ -66,13 +59,9 @@ def calculateScore(transactions):
                     scores['politics'] += 100
                 if item['pScore'] == "neutral" or item['pScore'] == "na":
                     scores['politics'] += 50
-                if len(foundTransactions) < 9:
-                    foundTransactions[transactions[i]['transaction_id']] = {'date': transactions[i]['date'], "eScore": int(item['eScore']), "sScore": int(item['sScore']), "gScore": int(item['gScore']), "pScore": (item['pScore']), "name": attempt['matchName'], "amount": transactions[i]['amount']}
                 break
         if not found:
-            #print("Not Found: " + transactions[i]['name'])
-            companyDB.not_found.update({"name": transactions[i]['name']}, {
-                                       '$inc': {"count": 1}}, upsert=True)
+            companyDB.not_found.update({"name": transactions[i]['name']}, {'$inc': {"count": 1}}, upsert=True)
         found = False
     if foundCount == 0:
         return("No Transactions Found")
@@ -80,14 +69,13 @@ def calculateScore(transactions):
     scores['social'] /= foundCount
     scores['politics'] /= foundCount
     scores['governance'] /= foundCount
-    scores['total'] = int(0.4 * scores['environmental'] +
-                          0.4 * scores['social'] + 0.2 * scores['governance'])
+    scores['total'] = int(0.4 * scores['environmental'] + 0.4 * scores['social'] + 0.2 * scores['governance'])
     return scores
 
 
 @linkPlaid_api.route("/link")
 def linkPlaid():
-    start = idk.time()
+    start = time.time()
     # print(companyDicionaryDB.scan())
     data = json.loads(request.data)
     access_token = None
@@ -98,31 +86,17 @@ def linkPlaid():
     user = db.users.find_one({'_id': ObjectId(data['id'])})
     access_token = user['plaid']['access_token']
     for i in range(6):
-        start_date = get_first_day(datetime.now(), 0, -(i + 1))
-        end_date = get_last_day(start_date)
+        start_date = '{:%Y-%m-%d}'.format(get_first_day(datetime.now(), 0, -(i + 1)))
+        end_date = '{:%Y-%m-%d}'.format(get_last_day(start_date))
         #print("Start: " + '{:%Y-%m-%d}'.format(start_date))
         #print("End: " + '{:%Y-%m-%d}'.format(end_date))
-
-        transactions_response = plaidClient.Transactions.get(access_token,
-                                                             '{:%Y-%m-%d}'.format(
-                                                                 start_date),
-                                                             '{:%Y-%m-%d}'.format(end_date))
+        transactions_response = plaidClient.Transactions.get(access_token, start_date, end_date)
         transactions = transactions_response['transactions']
         while len(transactions) < transactions_response['total_transactions']:
-            transactions_response = plaidClient.Transactions.get(access_token,
-                                                                 '{:%Y-%m-%d}'.format(
-                                                                     start_date),
-                                                                 '{:%Y-%m-%d}'.format(
-                                                                     end_date),
-                                                                 offset=len(
-                                                                     transactions)
-                                                                 )
+            transactions_response = plaidClient.Transactions.get(access_token, start_date, end_date, offset=len(transactions))
             transactions.extend(transactions_response['transactions'])
         scores = calculateScore(transactions)
-        print(scores)
         updateQuery = "scoreHistory." + str(start_date)
-        db.users.update({"_id": user['_id']}, {
-                        '$set': {updateQuery: scores, "initalizedHistory": True}}, upsert=False)
-    end = idk.time()
-    # print(transactions)
+        db.users.update({"_id": user['_id']}, {'$set': {updateQuery: scores, "initalizedHistory": True}}, upsert=False)
+    end = time.time()
     return str(end - start)
