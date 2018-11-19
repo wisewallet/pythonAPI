0f8ec5d1f4b734f24aa70c30df110263f86db8e8
William Brower <will@mywisewallet.com>
2018-11-19 14:15:05 -0500

    a

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from login import login_api
 from linkPlaid import linkPlaid_api
 from getscores import getscores_api
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-app.register_blueprint(signup_api)
-app.register_blueprint(linkPlaid_api)
-app.register_blueprint(login_api)
-app.register_blueprint(getscores_api)
+application.register_blueprint(signup_api)
+application.register_blueprint(linkPlaid_api)
+application.register_blueprint(login_api)
+application.register_blueprint(getscores_api)
 
-@app.route('/', methods=['GET'])
+@application.route('/', methods=['GET'])
 def index():
     return json.dumps("Hello World!")
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
