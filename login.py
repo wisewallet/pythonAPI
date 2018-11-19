170e82ecae3d09a56678cc5a8203f889a979724e
William Brower <will@mywisewallet.com>
2018-11-18 20:49:44 -0500

    a

--- login.py
+++ login.py
@@ -15,7 +15,7 @@ def login():
     data = json.loads(request.data)
     item = db.users.find_one({'email': data['email']})
     print(item["password"])
-    if bcrypt.checkpw(data["password"], "$2b$08$PUYZ/WHwG4lf2gLsp8O5R.XO96mvisnqbFmz39nFTfHkKbq2S1Emy"):
+    if bcrypt.checkpw(data["password"], item["password"].encode("UTF-8")):
         return("It Matches!")
     else:
         return("It Does not Match")
