2f6415bfa47ae5c79a9b0e5bbf7b9e491e5768a5
William Brower <will@mywisewallet.com>
2018-11-19 21:33:33 -0500

    a

--- pythonAPI.py
+++ pythonAPI.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 
 app.config["MONGO_URI"] = "mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/users?authSource=admin"
 app.mongo = PyMongo(app)
-db.authenticate(username, password, mechanism=mechanism)
 app.register_blueprint(signup_api)
 app.register_blueprint(linkPlaid_api)
 app.register_blueprint(login_api)
