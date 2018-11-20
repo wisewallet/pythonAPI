1c9c077d3378abf44c062aa3568a1eb7a83e211c
William Brower <will@mywisewallet.com>
2018-11-20 10:34:35 -0500

    a

--- signup.py
+++ signup.py
@@ -9,7 +9,7 @@ signup_api = Blueprint('signup_api', __name__)
 @signup_api.route("/signup")
 def signup():
     data = json.loads(request.data)
-    user = {"email": data['email'], "password": bcrypt.hashpw(data['password'].encode('UTF-8'), bcrypt.gensalt(
+    user = {"email": data['email'], "password": bcrypt.hashpw(data['password'].encode("UTF-8")), bcrypt.gensalt(
     )), "linked_plaid": False,"initalizedCurrent": False,"initalizedHistory": False, "name": data['name']}
     toReturn = current_app.userDB.db.users.insert_one(user).inserted_id
     return str(toReturn)
