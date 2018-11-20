88c45b77d15fc71bb9cdd92a93e53a36a7e0ea4b
William Brower <will@mywisewallet.com>
2018-11-19 21:19:04 -0500

    a

--- pythonAPI.py
+++ pythonAPI.py
@@ -7,8 +7,7 @@ from linkPlaid import linkPlaid_api
 from getscores import getscores_api
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/"
-app.mongo = PyMongo(app)
+app.mongo = PyMongo(app, uri="mongodb://dbadmin:xcdVRvVnykgGMeouDlTWEnVVh@69.55.55.54:27017/users")
 app.register_blueprint(signup_api)
 app.register_blueprint(linkPlaid_api)
 app.register_blueprint(login_api)
