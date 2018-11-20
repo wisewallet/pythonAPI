3fe8c532e024093c8890645d85eb27529a747990
William Brower <will@mywisewallet.com>
2018-11-20 10:59:40 -0500

    a

--- signup.py
+++ signup.py
@@ -2,14 +2,16 @@ from flask import Blueprint, request, current_app
 import simplejson as json
 import bcrypt
 from flask_pymongo import PyMongo
+from datetime import datetime, time, date
 
 signup_api = Blueprint('signup_api', __name__)
-
 
 @signup_api.route("/signup")
 def signup():
     data = json.loads(request.data)
-    user = {"email": data['email'], "password": bcrypt.hashpw(data['password'].encode("UTF-8"), bcrypt.gensalt(
-    )), "linked_plaid": False,"initalizedCurrent": False,"initalizedHistory": False, "name": data['name']}
+    created = datetime.now()
+    #start_date = '{:%Y-%m-%d}'.format(start_date_unformatted)
+    user = {"email": data['email'], "password": str(bcrypt.hashpw(data['password'].encode("UTF-8"), bcrypt.gensalt(
+    ))), "linked_plaid": False,"initalizedCurrent": False,"initalizedHistory": False, "name": data['name'], "created": created}
     toReturn = current_app.userDB.db.users.insert_one(user).inserted_id
     return str(toReturn)
